d1074010722127afc906ce319960aac3cdd9ec72
shueny <shuen29@gmail.com>
2021-07-21 02:51:48 +0000

    deploy to Github pages

---
+++ precache-manifest.e918b2a0c176d4ae702fd0285be4fdc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "286459cb33c3330b3394759f65545345",
+    "url": "/react-calendar/index.html"
+  },
+  {
+    "revision": "6a2c133ada8e8be3baf4",
+    "url": "/react-calendar/static/css/main.cba48064.chunk.css"
+  },
+  {
+    "revision": "965f0b2b22815b9599d1",
+    "url": "/react-calendar/static/js/2.b32801b1.chunk.js"
+  },
+  {
+    "revision": "308d2d97fd519422d20953c3e51f94e3",
+    "url": "/react-calendar/static/js/2.b32801b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a2c133ada8e8be3baf4",
+    "url": "/react-calendar/static/js/main.68d29bf5.chunk.js"
+  },
+  {
+    "revision": "e46bdf9d65c0534544de",
+    "url": "/react-calendar/static/js/runtime-main.9356b34f.js"
+  }
+]);
